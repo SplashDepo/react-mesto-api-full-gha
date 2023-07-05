62818536c2b06db5c0f0c4f5974eae512ca0ad74
d.8oulevard <d.8oulevard@yandex.ru>
2023-07-05 21:37:51 +0300

    изменил port

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ import NotFoundError from './errors/NotFoundError.js';
 import auth from './middlewares/auth.js';
 import { requestLogger, errorLogger } from './middlewares/logger.js';
 
-const { PORT = 3001, URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 mongoose.connect(URL)
   .then(() => console.log('Connect DB'))
